7a0f4307b2b0c6362336431811c12ea3bb812512
masoodw1 <masoodahmed@transpost.co>
2022-08-29 14:47:20 +0530

    auth code added

--- config/auth.php
+++ config/auth.php
@@ -59,11 +59,11 @@ return [
     |
     */
 
-    'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
-        ],
+    // 'providers' => [
+    //     'users' => [
+    //         'driver' => 'eloquent',
+    //         'model' => App\Models\User::class,
+    //     ],
 
         // 'users' => [
         //     'driver' => 'database',
