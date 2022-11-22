13fe53e832bce489ca7c36045a231957e7c96776
Rutuja-Parab <117453732+Rutuja-Parab@users.noreply.github.com>
2022-11-22 18:56:49 +0530

    Update RatesController.php

--- app/Http/Controllers/RatesController.php
+++ app/Http/Controllers/RatesController.php
@@ -79,11 +79,13 @@ class RatesController extends Controller
                     if(isset($rate['surcharge'])){
                         $surcharge = $rate['surcharge'];
                         $surchargeRate = $rate['rate_surcharge'];
+                        $sum =0;
                         for ($i=0; $i < count($rate['surcharge']); $i++) {
                             $rateArr[$i]['id'] = $i; 
                             $rateArr[$i]['code'] = $surcharge[$i]->Code;
                             $rateArr[$i]['name'] = $surcharge[$i]->Name;
                             $rateArr[$i]['amount'] = $surchargeRate[$i]->amount;
+                            $sum += $surchargeRate[$i]->amount;
                             $rateArr[$i]['currency'] = $surchargeRate[$i]->currency;
                             
                         }
@@ -94,7 +96,7 @@ class RatesController extends Controller
                     else{
                         $rate['surcharge'] = NULL;
                     }
-                
+                    $rate['total'] = $rate["_".$cargo_type] + $sum;
 
                   
                 
