ce9ceaeadad1a02b4f279b14b58ad0be9d07b2a2
Rutuja <rutujaparab@transpost.co>
2022-11-23 18:22:07 +0530

    pdf

--- app/Http/Controllers/RatesController.php
+++ app/Http/Controllers/RatesController.php
@@ -242,7 +242,7 @@ class RatesController extends Controller
         $customer = json_decode($data, true);
        $pdf = PDF::loadView('pdf', ['customer' => $customer]);
        $string = Str::random(8);
-       Storage::disk('public')->put('files/'.$string.'.pdf', $pdf->output());
+       Storage::put('files/'.$string.'.pdf', $pdf->output());
        $myFile = public_path('files/'.$string.'.pdf');
        //$pdf->SetTitle('Tranpost');
         //return view('pdf', ['customer' => $customer]);
