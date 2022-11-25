e2715a23733345d3b9f42e0b01062d816e6a73f9
Rutuja <rutujaparab@transpost.co>
2022-11-25 11:42:58 +0530

    pdf editing

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -300,8 +300,12 @@
                                 }
 
                             }
-                            print("Total:".$sum." USD");
-
+                            if(isset($cust['quantity'])){
+                            print("Total:".$sum*$cust['quantity']." USD");
+                            }
+                            else{
+                                print("Total:".$sum." USD");
+                            }
                         ?>
                     </td>
                 </tr>
@@ -363,7 +367,7 @@
                             print($cust['sellRate']*$cust['quantity']);
                         }
                         else{
-                            print($cust['amount']*1);
+                            print($cust['amount']*1." USD");
                         } ?>
                     </td>
                 </tr>
