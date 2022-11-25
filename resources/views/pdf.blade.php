3d1626ef0642535c3c67967b374252d54cb3af7a
Rutuja <rutujaparab@transpost.co>
2022-11-25 11:19:25 +0530

    pdf editing

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -259,7 +259,7 @@
                     </td>
                 </tr>
                 <?php $sum = 0;
-                if(empty($customer['additionalCosts'])){
+                if(!empty($customer['additionalCosts'])){
 
                  ?>
                 <tr style="background-color:gainsboro;">
