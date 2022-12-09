4b64c2dbfcca99cdd35a4a39539a08e4034b4af3
Rutuja <rutujaparab@transpost.co>
2022-12-09 19:14:46 +0530

    FInance

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -9,27 +9,37 @@ class FinanceController extends Controller
 {
     public function store(Request $request)
     {
-    $FinanceData = array(
-    'name'=> $request->name,
-    'commodity_name'=> $request->commodity_name,
-    'company_name'=> $request->company_name,
-    'gst'=> $request->gst,
-    'pan'=> $request->pan,
-    'iec_code'=> $request->iec_code,
-    'annual_turnover'=> $request->annual_turnover,
-    'product'=> $request->product,
-    'yoc'=> $request->yoc
-);
-    $isShipper= $request->isShipper;
+        $isShipper= $request->isShipper;
     $isForwarder= $request->isForwarder;
-        if($isShipper==TRUE){
-            $customer_type = 'Shipper';
-        }
-        else{
-            $customer_type = 'Forwarder';
-        }
-        $FinanceData['customer_type'] = $customer_type;
-        $finance = Freight_finance::create($FinanceData);
+
+    if($isShipper==TRUE){
+        $customer_type = 'Shipper';
+        $finance = Freight_finance::create([
+            'name'=> $request->name,
+            'commodity_name'=> $request->commodity_name,
+            'company_name'=> $request->company_name,
+            'iec_code'=> $request->iec_code,
+            'annual_turnover'=> $request->annual_turnover,
+            'product'=> $request->product,
+            'yoc'=> $request->yoc,
+            'customer_type'=> $customer_type
+    ]);
+}
+else{
+    $customer_type = 'Forwarder';
+        $finance = Freight_finance::create([
+            'name'=> $request->name,
+            'commodity_name'=> $request->commodity_name,
+            'company_name'=> $request->company_name,
+            'gst'=> $request->gst,
+            'pan'=> $request->pan,
+            'annual_turnover'=> $request->annual_turnover,
+            'product'=> $request->product,
+            'yoc'=> $request->yoc,
+            'customer_type'=> $customer_type
+    ]);
+}
+    $finance->save();
         return response()->json([
             'status' => 'success',
             'message' => 'Created successfully',
